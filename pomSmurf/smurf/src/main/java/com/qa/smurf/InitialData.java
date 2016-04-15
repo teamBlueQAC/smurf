dd10f1ec491e76dd188eacce7e29a5123c3235fd
unknown <tom@procterweb.co.uk>
2016-04-15 14:06:09 +0000

    Create initial data template

---
+++ pomSmurf/smurf/src/main/java/com/qa/smurf/InitialData.java
@@ -0,0 +1,37 @@
+package com.qa.smurf;
+
+public class InitialData {
+
+	//Supplier
+	
+	//Address
+	
+	//Payment
+	
+	//Orders
+	
+	//User
+	
+	//Credit
+	
+	//OrderStatus
+	
+	//LineItems
+	
+	//WishlistEntry
+	
+	//EmployeeUser
+	
+	//AccountType
+	
+	//SupplyOrder
+	
+	//Products
+	
+	//ProductType
+	
+	//ProdCat
+	
+	//Category
+	
+}
