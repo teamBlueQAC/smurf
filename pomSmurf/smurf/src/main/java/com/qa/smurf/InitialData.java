0028bd0a0ae16795c002a759c4976543475e0e79
unknown <tom@procterweb.co.uk>
2016-04-15 16:03:32 +0000

    Add getOrders(), getSuplierOrder() and getOrderStatus() methods to InitialData class

--- pomSmurf/smurf/src/main/java/com/qa/smurf/InitialData.java
+++ pomSmurf/smurf/src/main/java/com/qa/smurf/InitialData.java
@@ -1,4 +1,9 @@
 package com.qa.smurf;
+/**
+ * Author - Tom
+ */
+import java.util.ArrayList;
+import java.util.Date;
 
 public class InitialData {
 
@@ -9,12 +14,33 @@ public class InitialData {
 	//Payment
 	
 	//Orders
+	public ArrayList<Order> getOrder(){
+		ArrayList<Order> orders = new ArrayList<Order>();
+		double[] price = {10.10, 20.20, 30.30, 40.40, 50.50};
+		
+		for (int i = 0; i < 5; i++){
+			orders.add(new Order(price[i], new Date(), new Date(), getPayment().get(i), getAddress().get(i), getUser().get(i), getOrderStatus().get(i)));
+		}
+		
+		return orders;
+	}
 	
 	//User
 	
 	//Credit
 	
 	//OrderStatus
+	public ArrayList<OrderStatus> getOrderStatus(){
+		ArrayList<OrderStatus> orderStatus = new ArrayList<OrderStatus>();
+		
+		String[] name = { "Cancelled", "Delivered", "Dispatched", "Awaiting Confirmation", "Dispatched" };
+		for (int i = 0; i < 5; i++){
+			orderStatus.add(new OrderStatus(name[i]));
+		}
+		
+		return orderStatus;
+	}
+	
 	
 	//LineItems
 	
@@ -25,6 +51,16 @@ public class InitialData {
 	//AccountType
 	
 	//SupplyOrder
+	public ArrayList<SupplierOrder> getSupplierOrder(){
+		ArrayList<SupplierOrder> supplierOrder = new ArrayList<SupplierOrder>();
+		int received[] = { 1, 2, 3, 4, 4 };
+		
+		for (int i = 0; i < 5; i++){
+			supplierOrder.add(new SupplierOrder(getProduct().get(i), getOrderStatus().get(i), getSupplier().get(i), i, new Date(), received[i]));
+		}
+		
+		return supplierOrder;
+	}
 	
 	//Products
 	
