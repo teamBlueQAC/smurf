f4de999e0dd13e92127a884672cab1346d32ed3b
NathanOrme <nathan.orme23@gmail.com>
2016-04-27 15:34:51 +0100

    Added AccountType Offline Repository

---
+++ pomSmurf/smurf/src/main/java/com/qa/smurf/repositories/offline/AccountTypeRepositoryOffline.java
@@ -0,0 +1,72 @@
+package com.qa.smurf.repositories.offline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.qa.smurf.InitialData;
+import com.qa.smurf.entities.Credit;
+import com.qa.smurf.entities.Order;
+import com.qa.smurf.entities.Payment;
+import com.qa.smurf.repositories.AccountTypeRepository;
+import com.qa.smurf.util.AccountType;
+
+
+
+public class AccountTypeRepositoryOffline implements AccountTypeRepository{
+	@Inject
+	private InitialData initialData;
+	
+	@Override
+	public void persistAccountType(AccountType accountType) {
+		initialData.addAccountType(accountType);
+		
+	}
+
+	@Override
+	public void persistAccountTypes(List<AccountType> accountType) {
+		for(AccountType a: accountType){
+			initialData.addAccountType(a);;
+		}	
+		
+	}
+
+	@Override
+	public AccountType findByID(int id) {
+		for(AccountType a: initialData.getAccountTypes()){
+			if(a.getId() == id){
+				return a;
+			}
+		}
+	return null;
+	}
+
+	@Override
+	public ArrayList<AccountType> getAccountTypes() {
+		return initialData.getAccountTypes();
+	}
+
+	@Override
+	public void updateAccountType(AccountType accountType) {
+		ArrayList<AccountType> accountTypes = initialData.getAccountTypes();
+		for(int i=0; i<accountTypes.size(); i++) {
+			if(accountTypes.get(i).getId() == accountType.getId())
+				accountTypes.set(i, accountType);
+		}
+		initialData.setAccountType(accountTypes);
+		
+	}
+
+	@Override
+	public void removeAccountType(AccountType accountType) {
+		ArrayList<AccountType> accountTypes = initialData.getAccountTypes();
+		for(int i=0; i<accountTypes.size(); i++) {
+			if(accountTypes.get(i).equals(accountType))
+				accountTypes.remove(i);
+		}
+		initialData.setAccountType(accountTypes);
+		
+	}
+
+}
