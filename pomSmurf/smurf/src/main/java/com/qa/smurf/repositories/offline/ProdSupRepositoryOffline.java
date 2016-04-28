d4e27b6a9c0175fbe70adce380c05fbde22cae18
OmarDMorrison <OmarDMorrison@hotmail.co.uk>
2016-04-28 10:43:08 +0100

    Started the offline Repositories but incomplete

---
+++ pomSmurf/smurf/src/main/java/com/qa/smurf/repositories/offline/ProdSupRepositoryOffline.java
@@ -0,0 +1,17 @@
+package com.qa.smurf.repositories.offline;
+
+/**
+ * Author - Omar
+ */
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import com.qa.smurf.InitialData;
+import com.qa.smurf.repositories.ProdSupRepository;
+
+
+public class ProdSupRepositoryOffline implements ProdSupRepository{
+
+}
