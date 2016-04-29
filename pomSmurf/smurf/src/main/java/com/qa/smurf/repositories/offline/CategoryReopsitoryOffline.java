42649aa826df69e108d856a4cd8298d3df82562e
teamBlueQAC <qac2016>
2016-04-29 11:09:21 +0000

    Completed offline repository.

--- pomSmurf/smurf/src/main/java/com/qa/smurf/repositories/offline/CategoryReopsitoryOffline.java
+++ pomSmurf/smurf/src/main/java/com/qa/smurf/repositories/offline/CategoryReopsitoryOffline.java
@@ -3,45 +3,57 @@ package com.qa.smurf.repositories.offline;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
+import com.qa.smurf.InitialData;
 import com.qa.smurf.entities.Category;
 import com.qa.smurf.repositories.CategoryRepository;
 
 public class CategoryReopsitoryOffline implements CategoryRepository {
+	
+	@Inject
+	private InitialData initialData;
 
 	@Override
 	public void persistCategory(Category c) {
-		// TODO Auto-generated method stub
-
+		initialData.addCategory(c);
 	}
 
 	@Override
 	public void persistCategorys(List<Category> c) {
-		// TODO Auto-generated method stub
-
+		for(Category cat : c) {
+			persistCategory(cat);
+		}
 	}
 
 	@Override
 	public Category findByID(long id) {
-		// TODO Auto-generated method stub
+		for(Category cat : initialData.getCategories()) {
+			if(cat.getID() == id) {
+				return cat;
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public ArrayList<Category> getCategories() {
-		// TODO Auto-generated method stub
-		return null;
+		return initialData.getCategories();
 	}
 
 	@Override
 	public void updateCategory(Category c) {
-		// TODO Auto-generated method stub
-
+		for(int i = 0; i < initialData.getCategories().size(); i++) {
+			if(initialData.getCategories().get(i).getID() == c.getID()) {
+				initialData.getCategories().set(i, c);
+				break;
+			}
+		}
 	}
 
 	@Override
 	public void removeCategory(Category c) {
-		// TODO Auto-generated method stub
-
+		initialData.getCategories().remove(c);
 	}
 
 }
