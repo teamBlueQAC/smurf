e7ad8720859b5bf3488931ec6edaf83ed963e5a3
Tom Procter <tom@procterweb.co.uk>
2016-05-03 12:30:44 +0100

    Add product service find by ID method

--- pomSmurf/smurf/src/main/java/com/qa/smurf/service/ProductService.java
+++ pomSmurf/smurf/src/main/java/com/qa/smurf/service/ProductService.java
@@ -1,12 +1,16 @@
 package com.qa.smurf.service;
 
+import javax.inject.Inject;
+
 import com.qa.smurf.entities.Product;
+import com.qa.smurf.repositories.ProductRepository;
 
 public class ProductService {
+	@Inject
+	ProductRepository productRepository;
 
 	public Product findByProductId(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return productRepository.findByID(id);
 	}
 
 }
