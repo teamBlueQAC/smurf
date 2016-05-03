1dbc66486f7adfb06a2969501df0cf5e7ed55d3b
NathanOrme <nathan.orme23@gmail.com>
2016-05-03 17:22:14 +0100

    Worked on Order Service

--- pomSmurf/smurf/src/main/java/com/qa/smurf/service/OrderService.java
+++ pomSmurf/smurf/src/main/java/com/qa/smurf/service/OrderService.java
@@ -1,9 +1,29 @@
 package com.qa.smurf.service;
 
+import java.util.Date;
+
+import javax.inject.Inject;
+
+import com.qa.smurf.entities.Address;
+import com.qa.smurf.entities.LineItems;
 import com.qa.smurf.entities.Order;
+import com.qa.smurf.entities.Payment;
+import com.qa.smurf.entities.Product;
 import com.qa.smurf.entities.User;
+import com.qa.smurf.repositories.OrderRepository;
+import com.qa.smurf.repositories.ProductRepository;
+import com.qa.smurf.repositories.UserRepository;
 
 public class OrderService {
+
+	@Inject
+	ProductRepository productRepository;
+
+	@Inject
+	OrderRepository orderRepository;
+
+	@Inject
+	UserRepository userRepository;
 
 	public User getCurrentUser(long userId) {
 		// TODO Auto-generated method stub
@@ -11,18 +31,37 @@ public class OrderService {
 	}
 
 	public void addToBasket(long productId, long userId) {
+		Product product = productRepository.findByID(productId);
+		Order order = orderRepository.getUsersPendingOrder(userId);
+		if(order != null){
+			boolean foundLineItem = false;
+			for (LineItems li : order.getOrderLineItems()){
+				if(li.getProduct().getId() == productId){
+					li.setQuantity(li.getQuantity()+1);
+					foundLineItem = true;
+				}
+				order.addLineItem(new LineItems(order, product, 1, product.getPrice(), product.getQuantityAvailable()));
+			}
+
+		} else{
+			order = new Order(123, new Date(), new Date(), 
+					new Payment(null, null, null, null, null, null), 
+					new Address("Place", "Postcode"), 
+					userRepository.findByID(userId), null);
+		}
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void updateOrder(Order order, long userId) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void clearBasket(long userId) {
-		// TODO Auto-generated method stub
-		
+		Order order = orderRepository.getUsersPendingOrder(userId);
+
+
 	}
 
 	public Order getUsersPendingOrder(long userId) {
@@ -31,23 +70,38 @@ public class OrderService {
 	}
 
 	public float calcOrderTotalPending(long userId) {
-		// TODO Auto-generated method stub
-		return 0;
+		float total = 0;
+		Order order = orderRepository.getUsersPendingOrder(userId);
+		if(order != null){
+			for (LineItems li : order.getOrderLineItems()){
+				total=(float) li.getSubtotal();
+			}
+		}
+		return total;
 	}
 
 	public void placeOrder(Order order, long userId) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void removeFromBasket(long productId, long userId) {
-		// TODO Auto-generated method stub
-		
+		Product product = productRepository.findByID(productId);
+		Order order = orderRepository.getUsersPendingOrder(userId);
+		if(order != null){
+			boolean foundLineItem = false;
+			for (LineItems li : order.getOrderLineItems()){
+				if(li.getProduct().getId() == productId){
+					order.removeLineItem(li);
+				}
+			}
+
+		} 
 	}
 
 	public void getLineItems(Order order, long userId) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
