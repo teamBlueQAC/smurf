e5ce9dd703ee38675fafed26090420e752e2f453
willseaford <willseaford@gmail.com>
2016-05-04 16:16:36 +0000

    modified service and controller to fit roles

--- src/main/java/com/qa/smurf/controllers/PaymentController.java
+++ src/main/java/com/qa/smurf/controllers/PaymentController.java
@@ -18,56 +18,31 @@ public class PaymentController {
 		@Inject
 		PaymentService paymentService; 
 		
-		String error;
-		
-		/*Method checks to see if the card number is 16 digits long and only contains numbers*/
-		public String validateCardNumber(long cardNumber){
-			if(Long.valueOf(cardNumber).toString().length() != 16){
-				error = "CARD LENGTH MUST BE 16 DIGITS;" ;
-				return error;
+		public Boolean validateCardName(String name){
+			if(paymentService.validateCardName(name)){
+				return true;
 			}
-			String pattern = "[//d]+";
-			Pattern regexPattern = Pattern.compile(pattern);
-			Matcher matcher = regexPattern.matcher(Long.valueOf(cardNumber).toString());
-			if(!matcher.find()){
-				error = error + "FIELD MUST ONLY CONTAIN DIGITS;";
-				return error;
-			}
-			return null;
+			return false;
 		}
 		
-		/*Method checks the card name to make sure it only contains alpha-numerical characters*/
-		public String validateCardName(String name){
-			String pattern = "^[a-zA-Z0-9]*$";
-			Pattern regexPattern = Pattern.compile(pattern);
-			Matcher matcher = regexPattern.matcher(name);
-			if(!matcher.find()){
-				error = "NAME MUST ONLY CONTAIN ALPHANUMERICAL CHARACTERS";
-				return error;
+		public Boolean validateCardNumber(long cardNumber){
+			if(paymentService.validateCardNumber(cardNumber)){
+				return true;
 			}
-			return null;
+			return false;
 		}
 		
-		/*Method checks the card type to accept only credit or debit*/
-		public String validateCardType(String cardType){
-			
-			if(!cardType.toLowerCase().equals("credit")||!cardType.toLowerCase().equals("debit")){
-				error = "PAYMENT METHOD MUST BE EITHER CREDIT OR DEBIT CARD";
-				return null;
+		public Boolean validateExpiryDate(String expiryDate){
+			if(paymentService.validateExpiryDate(expiryDate)){
+				return true;
 			}
-			return null;
+			return false;
 		}
 		
-		/*method checks the expiry date and makes sure it is in the correct date format*/
-		public String validateExpiryDate(String expiryDate){
-			String pattern = "[0-9][0-9][0-9][0-9]";
-			expiryDate.replace("/", "");
-			Pattern regexPattern = Pattern.compile(pattern);
-			Matcher matcher = regexPattern.matcher(expiryDate);
-			if(!matcher.find()){
-				error = "EXPIRY DATE MUST MATCH THE FORMAT MM/YY";
-				return error;
+		public Boolean validateCardType(String cardType){
+			if(paymentService.validateCardType(cardType)){
+				return true;
 			}
-			return null;
+			return false;
 		}
 }
--- src/main/java/com/qa/smurf/service/PaymentService.java
+++ src/main/java/com/qa/smurf/service/PaymentService.java
@@ -1,5 +1,55 @@
 package com.qa.smurf.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PaymentService {
 
+	//String error;
+	
+	/*Method checks to see if the card number is 16 digits long and only contains numbers*/
+	public Boolean validateCardNumber(long cardNumber){
+		if(Long.valueOf(cardNumber).toString().length() != 16){
+			
+			return false;
+		}
+		String pattern = "[//d]+";
+		Pattern regexPattern = Pattern.compile(pattern);
+		Matcher matcher = regexPattern.matcher(Long.valueOf(cardNumber).toString());
+		if(!matcher.find()){
+			return false;
+		}
+		return true;
+	}
+	
+	/*Method checks the card name to make sure it only contains alpha-numerical characters*/
+	public Boolean validateCardName(String name){
+		String pattern = "^[a-zA-Z0-9]*$";
+		Pattern regexPattern = Pattern.compile(pattern);
+		Matcher matcher = regexPattern.matcher(name);
+		if(!matcher.find()){
+			return false;
+		}
+		return true;
+	}
+	
+	/*Method checks the card type to accept only credit or debit*/
+	public Boolean validateCardType(String cardType){
+		if(!cardType.toLowerCase().equals("credit")||!cardType.toLowerCase().equals("debit")){
+			return false;
+		}
+		return true;
+	}
+	
+	/*method checks the expiry date and makes sure it is in the correct date format*/
+	public Boolean validateExpiryDate(String expiryDate){
+		String pattern = "[0-9][0-9][0-9][0-9]";
+		expiryDate.replace("/", "");
+		Pattern regexPattern = Pattern.compile(pattern);
+		Matcher matcher = regexPattern.matcher(expiryDate);
+		if(!matcher.find()){
+			return false;
+		}
+		return true;
+	}
 }
