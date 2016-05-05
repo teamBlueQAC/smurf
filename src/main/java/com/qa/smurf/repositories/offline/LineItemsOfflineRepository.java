548e657badbb837050dce1c1b4f3d9393abc294b
willseaford <willseaford@gmail.com>
2016-05-05 16:47:10 +0000

    Fixed initialData parseException problems in Credit, employeeUser

--- src/main/java/com/qa/smurf/repositories/offline/LineItemsOfflineRepository.java
+++ src/main/java/com/qa/smurf/repositories/offline/LineItemsOfflineRepository.java
@@ -63,21 +63,13 @@ public class LineItemsOfflineRepository implements LineItemsRepository {
 
 	public ArrayList<LineItems> getLineItems() {
 		ArrayList<LineItems> lineitems = new ArrayList<LineItems>();
-		try {
-			lineitems = initialData.getLineItems();
-		} catch (ParseException pe) {
-			System.err.println();
-		}
+		lineitems = initialData.getLineItems();
 		return lineitems;
 	}
 
 	public LineItems readLineItems(LineItems p) {
 		ArrayList<LineItems> lineitems = new ArrayList<LineItems>();
-		try {
-			lineitems = initialData.getLineItems();
-		} catch (ParseException pe) {
-			System.err.println();
-		}
+		lineitems = initialData.getLineItems();
 		for (int i = 0; i < lineitems.size(); i++) {
 			if (lineitems.get(i).equals(p)) {
 				return lineitems.get(i);
