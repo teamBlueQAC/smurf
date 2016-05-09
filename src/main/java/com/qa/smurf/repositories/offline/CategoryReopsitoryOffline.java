407fbdea15e9f845a149800194fe05d9bba5d852
ND8 <nabil_dalal@hotmail.com>
2016-05-09 14:19:21 +0100

    realised offline category repository was unfinished so updated the offline repo

--- src/main/java/com/qa/smurf/repositories/offline/CategoryReopsitoryOffline.java
+++ src/main/java/com/qa/smurf/repositories/offline/CategoryReopsitoryOffline.java
@@ -3,44 +3,69 @@ package com.qa.smurf.repositories.offline;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
+import com.qa.smurf.InitialData;
 import com.qa.smurf.entities.Category;
+import com.qa.smurf.entities.Product;
 import com.qa.smurf.repositories.CategoryRepository;
 
 public class CategoryReopsitoryOffline implements CategoryRepository {
 
+	@Inject
+	private InitialData initialData;
+
 	@Override
 	public void persistCategory(Category c) {
-		// TODO Auto-generated method stub
+		initialData.addCategory(c);
 
 	}
 
 	@Override
 	public void persistCategorys(List<Category> c) {
-		// TODO Auto-generated method stub
+		for (Category cat : c) {
+			persistCategory(cat);
+		}
 
 	}
 
 	@Override
 	public Category findByID(long id) {
-		// TODO Auto-generated method stub
+		ArrayList<Category> categories = initialData.getCategories();
+		for (int i = 0; i < categories.size(); i++) {
+			if (categories.get(i).getID() == id) {
+				return categories.get(i);
+			}
+		}
 		return null;
+
 	}
 
 	@Override
 	public ArrayList<Category> getCategories() {
-		// TODO Auto-generated method stub
-		return null;
+		return initialData.getCategories();
 	}
 
 	@Override
 	public void updateCategory(Category c) {
-		// TODO Auto-generated method stub
-
+		ArrayList<Category> categories = initialData.getCategories();
+		for (int i = 0; i < categories.size(); i++) {
+			if (categories.get(i).getID() == c.getID()) {
+				categories.set(i, c);
+				break;
+			}
+		}
 	}
 
 	@Override
 	public void removeCategory(Category c) {
-		// TODO Auto-generated method stub
+		ArrayList<Category> categories = initialData.getCategories();
+		for (int i = 0; i < categories.size(); i++) {
+			if (categories.get(i).equals(c)) {
+				categories.remove(i);
+				break;
+			}
+		}
 
 	}
 
