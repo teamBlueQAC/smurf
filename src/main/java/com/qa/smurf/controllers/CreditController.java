f4bbf6cff0f714cac2e37d47a410d01181ea87d7
Tom Procter <tom@procterweb.co.uk>
2016-05-09 14:48:25 +0100

    Adjust credit controller

--- src/main/java/com/qa/smurf/controllers/CreditController.java
+++ src/main/java/com/qa/smurf/controllers/CreditController.java
@@ -1,17 +1,30 @@
 package com.qa.smurf.controllers;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.RequestScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import com.qa.smurf.entities.Credit;
-import com.qa.smurf.entities.User;
 import com.qa.smurf.service.CreditService;
 
+@Named(value = "credit")
+@RequestScoped
 public class CreditController {
 	
 	@Inject
-	CreditService creditService;
+	private CreditService creditService;
+	@Inject
+	private CurrentUser currentUser;
 	
-	public Credit findByUser(User user){
-		return creditService.findByUser(user);
+	private Credit credit;
+	
+	@PostConstruct
+	public void init(){
+		credit = creditService.findByUserId(currentUser.getUserId());
+	}
+	
+	public Credit getCredit(){
+		return this.credit;
 	}
 }
