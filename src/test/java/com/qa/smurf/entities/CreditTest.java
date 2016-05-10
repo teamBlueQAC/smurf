6135e021e8b52dda111bda028b961ba0ee04437c
Elliot5849 <Elliot.Marsh@qa.com>
2016-05-10 12:07:48 +0100

    Changing catalogue and product

---
+++ src/test/java/com/qa/smurf/entities/CreditTest.java
@@ -0,0 +1,47 @@
+package com.qa.smurf.entities;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class CreditTest extends TestCase {
+	private Credit credit;	
+	
+	public CreditTest(String name) {
+		super(name);
+		credit = new Credit(new User(0, "", "", "", "", null, true, new Address("", ""), null), 0, 0, 1);
+	}
+
+	@Test
+	public void testGetUser() {
+		assertNotNull(credit.getUser());
+	}
+
+//	public void testSetUserID() {
+//		fail("Not yet implemented");
+//	}
+//
+//	public void testGetAmount() {
+//		fail("Not yet implemented");
+//	}
+//
+//	public void testSetAmount() {
+//		fail("Not yet implemented");
+//	}
+//
+//	public void testGetAmountRemaining() {
+//		fail("Not yet implemented");
+//	}
+//
+//	public void testSetAmountRemaining() {
+//		fail("Not yet implemented");
+//	}
+//
+//	public void testGetQuarter() {
+//		fail("Not yet implemented");
+//	}
+//
+//	public void testSetQuarter() {
+//		fail("Not yet implemented");
+//	}
+}
