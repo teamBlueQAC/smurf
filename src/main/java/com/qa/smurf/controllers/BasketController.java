beaddcfa98ab38c3c80c756d6d0864c29644678b
NathanOrme <nathan.orme23@gmail.com>
2016-05-11 14:26:38 +0100

    Reworked basket so the page works when logged in

--- src/main/java/com/qa/smurf/controllers/BasketController.java
+++ src/main/java/com/qa/smurf/controllers/BasketController.java
@@ -20,35 +20,32 @@ import com.qa.smurf.service.OrderService;
 @Named(value = "basket")
 @RequestScoped
 public class BasketController {
-
-	@Inject
-	private OrderService orderService;
-	@Inject
-	private CurrentUser currentUser;
+	@Inject	private OrderService orderService;
+	@Inject	private CurrentUser currentUser;
 	private User user;
 	private Order order;
 	private List<LineItems> lineItems;
 
 	public String removeProductFromBasket(long productId) {
-		orderService.removeFromBasket(productId, user.getId());
+		orderService.removeFromBasket(productId, currentUser.getUserId());
 		return "basket";
 	}
 
 	public String updateQuantity() {
-		orderService.updateQuantity(order, user.getId());
+		orderService.updateQuantity(order, currentUser.getUserId());
 
 		return "basket";
 	}
 
 	public String clearBasket() {
-		orderService.clearBasket(user.getId());
+		orderService.clearBasket(currentUser.getUserId());
 		return "basket";
 	}
 
 	// gets called when adding a product
 	// calls the orderService to add a product to the basket.
 	public void addToBasket(long productId) {
-		orderService.addToBasket(productId, user.getId());
+		orderService.addToBasket(productId, currentUser.getUserId());
 	}
 
 	public void getLineItems(Order order) {
