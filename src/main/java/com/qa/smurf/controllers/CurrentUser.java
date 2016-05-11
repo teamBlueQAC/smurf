57274411198d2472f619e6d7a442337e3835b88a
amjad121092 <amjad1210@live.co.uk>
2016-05-11 12:31:27 +0100

    Session fix for logging

--- src/main/java/com/qa/smurf/controllers/CurrentUser.java
+++ src/main/java/com/qa/smurf/controllers/CurrentUser.java
@@ -11,7 +11,27 @@ public class CurrentUser implements Serializable {
 
 	private static final long serialVersionUID = 1647685390330337075L;
 	
-	private long userId = 1;
+	private long userId = -1;
+	
+	private String username;
+	
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 	public long getUserId() {
 		return userId;
