f205a2914de31b8afbbcf88a18ffc19c605645f8
NathanOrme <nathan.orme23@gmail.com>
2016-05-17 09:14:42 +0100

    Streamlined gewtting payment in payment controller

--- src/main/java/com/qa/smurf/controllers/PaymentController.java
+++ src/main/java/com/qa/smurf/controllers/PaymentController.java
@@ -135,8 +135,7 @@ public class PaymentController {
 	}
 
 	public Payment getPayment() {
-		User paymentUser = paymentService.getCurrentUser(currentUser.getUserId());
-		payment = paymentService.getPayment(paymentUser);
+		payment = paymentService.getPayment(paymentService.getCurrentUser(currentUser.getUserId()));
 		return payment;
 	}
 
